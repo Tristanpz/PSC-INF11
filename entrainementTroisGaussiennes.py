f0d35ff399da963760235c49fe55036e0057ca0c
Tristanpz <121450578+Tristanpz@users.noreply.github.com>
2025-02-05 16:39:00 +0100

    Add files via upload

--- entrainementTroisGaussiennes.py
+++ entrainementTroisGaussiennes.py
@@ -7,8 +7,6 @@ Created on Wed Feb  5 15:39:52 2025
 
 import torch
 import numpy as np
-import scipy.stats as sps
-import matplotlib.pyplot as plt
 
 import reseauPytorch
 
@@ -17,11 +15,10 @@ def genDonneesGaussiennes() :
   al = np.random.randint(0,3)
   return al, np.random.multivariate_normal(m[al], s)
 
-
 N = 100
 
-X = np.zeros((N))
-y = np.zeros((N))
+X = np.zeros((N,2))
+y = np.zeros((N,3))
     
 def format_res(i):
     if i==0:
@@ -33,16 +30,19 @@ def format_res(i):
     
 for i in range(N) :
     res, donnee = genDonneesGaussiennes()
-    X[i] = donnee
-    y[i] = format_res(res)
+    X[i,0] = donnee[0]
+    X[i,0] = donnee[1]
+    for j in range(3) : 
+        y[i,j] = format_res(res)[j]
 
-X_tensor = torch.tensor(np.transpose(np.atleast_2d(X)), dtype=torch.float32)
-y_tensor = torch.tensor(np.transpose(np.atleast_2d(y)), dtype=torch.float32)
+X_tensor = torch.tensor(X, dtype=torch.float32)
+y_tensor = torch.tensor(y, dtype=torch.float32)
 
 num_epochs = 1000
 nbBatch = 2
 
-model = reseauPytorch.creerReseau([1,3,2,3])
+model = reseauPytorch.creerReseau([2,3,2,3])
 reseauPytorch.entrainerReseau(model, X_tensor, y_tensor, num_epochs, nbBatch)
 
-torch.save(model, "model_3g.pth") 
+
+torch.save(model, "model_3g.pth")
